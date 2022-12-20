a96315fbecdc5d354d0cba0d268f5d35ec0cc9c6
Chris Amora <christiano.amora@gmail.com>
2022-12-19 21:40:09 -0300

    add kickstart

--- nvim/.config/nvim/lua/keymaps.lua
+++
@@ -1,2 +0,0 @@
-vim.api.nvim_set_keymap('n', '<C-p>', '<cmd>lua require("telescope.builtin").find_files()<Cr>', {noremap = true, silent = true})
-
--- nvim/.config/nvim/lua/set.lua
+++ nvim/.config/nvim/lua/set.lua
@@ -5,9 +5,7 @@ vim.opt.relativenumber = true
 
 vim.opt.errorbells = false
 
-vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- Case insensitive searching UNLESS /C or capital in search
@@ -16,33 +14,12 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
-vim.opt.swapfile = false
-vim.opt.backup = false
--- vim.opt.undodir = "undodir"
--- vim.opt.undofile = true
-
-vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
 
 -- Give more space for displaying messages.
 vim.opt.cmdheight = 1
 
--- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
--- delays and poor user experience.
-vim.opt.updatetime = 50
-
 -- Don't pass messages to |ins-completion-menu|.
 vim.opt.shortmess:append("c")
-
--- vim.opt.colorcolumn = "80"
-
-vim.g.mapleader = " "
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'

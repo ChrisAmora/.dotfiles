d13157a1102c02e4a47724040006e33de8152980
Chris Amora <christiano.amora@gmail.com>
2022-12-23 09:51:26 -0300

    new keymaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,5 +1,8 @@
+local map = require("utils").map
 local builtin = require('telescope.builtin')
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = '[P]re[V]iews'})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {desc = '[G]it [F]ind'})
 vim.keymap.set('n', '<leader>km', builtin.keymaps, {desc = '[K]ey[M]aps'})
 
+
+map("n", "<leader>w", "<C-w>", { silent = true})

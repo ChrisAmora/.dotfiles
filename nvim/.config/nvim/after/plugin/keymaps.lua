60200d348ff6d5dd87af6d6873c9f3dcf0970487
ChrisAmora <christiano.amora@gmail.com>
2022-12-26 16:23:59 -0300

    add LazyGit Shortcut

--- nvim/.config/nvim/after/plugin/keymaps.lua
+++ nvim/.config/nvim/after/plugin/keymaps.lua
@@ -11,9 +11,10 @@ end
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = '[P]re[V]iews'})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {desc = '[G]it [F]ind'})
 vim.keymap.set('n', '<leader>km', builtin.keymaps, {desc = '[K]ey[M]aps'})
-vim.keymap.set('n', '<leader>lg', builtin.live_grep, {desc = '[L]ive[Grep]'})
+-- vim.keymap.set('n', '<leader>lg', builtin.live_grep, {desc = '[L]ive[Grep]'})
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
 vim.keymap.set("n", "<leader>ff", ":Format<CR>") -- Format Files
+vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>") -- Format Files
 
 -- For Splits on Windows
 map("n", "<leader>w", "<C-w>", { silent = true})
